dd975be3d7c95e4907101b9ab27d9a1a5a35129c
Amreta09 <52856519+Amreta09@users.noreply.github.com>
2019-07-19 06:16:32 +0530

    Update .c

--- .c
+++ .c
@@ -2,10 +2,19 @@
 #include<string.h>
 int main()
 {
-char arr[100];
-char a;
-get(arr);
-a=strrev(arr);
-printf("%s",a);
+char str[100],rev[100];
+int i,count=0,j;
+ 
+gets(str);
+while(str[count]!=0){
+  count++;
+}
+  j=count-1;
+  for (i = 0; i < count; i++) {
+rev[i] = str[j];
+j--;
+  }
+  rev[i]='\0';
+printf("%s",rev);
 return 0;
 }
